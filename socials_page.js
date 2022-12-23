f49307050356cfc12ef322de2a753839c3d8242e
Shreya <73158356+5hreya@users.noreply.github.com>
2022-12-23 00:02:18 -0800

    Add files via upload

---
+++ socials_page.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNTMRWLukekXJdAJC7yilguo3J-tmrVZk",
+    authDomain: "socials-91ccb.firebaseapp.com",
+    projectId: "socials-91ccb",
+    storageBucket: "socials-91ccb.appspot.com",
+    messagingSenderId: "182115637895",
+    appId: "1:182115637895:web:465a475fa6256c1d483622",
+    measurementId: "G-LPVR491BNJ"
+  };
+  firebase.initializeApp(firebaseConfig)
+
+  
